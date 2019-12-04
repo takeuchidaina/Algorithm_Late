75bbc659ed105b025ec15d222dc6c729618fe12c
takeuchidaina <yda.takeuchidaina@gmail.com>
2019-12-04 15:13:48 +0900

    [update]バーストの判定を追加 バースト後はボタンが出ないように

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 
 $isGameEnd = false;     //true ゲーム終了 false 進行中
+$isBurst = false;       //true バースト false 21以下
 $deck   = array();      //デッキ
 $player  = array();     //手札
 
@@ -38,14 +39,30 @@ function EnemyTurn(){
 #手札の点数
 ##################################################
 function HandScore($_someOne){
+  $totalScore = 0;
+  foreach ($_someOne as $deck) {
+    //11以上の数字は10へ
+    if($deck['num'] >= 11){
+      $totalScore += 10;
+    }
+    else{
+      $totalScore += $deck['num'];
+    }
+  }
 
+  return $totalScore;
 }
 ##################################################
 #バーストしているかチェックをする
 #していればゲーム終了
 ##################################################
 function BurstCheck($_someOne){
-
+  if(HandScore($_someOne) > 21){
+    return true;    //バースト
+  }
+  else{
+    return false;
+  }
 }
 
 #################
@@ -85,9 +102,19 @@ $_SESSION['player'] = $player;
 #################
 #手札を表示
 #################
-echo "【手札】<br>";
+echo "【手札】SCORE:";
+$score = HandScore($player);
+echo $score."<br>";
+
+//バーストしているか確認
+if(BurstCheck($player) == true){
+  echo "BURST<br>";
+  $isBurst = true;
+}
+
+//手札の中身を表示
 foreach($player as $deck){
-  echo $deck['num']."<br>";
+  echo $deck['num']."　　";
 }
 ?>
 
@@ -99,7 +126,10 @@ foreach($player as $deck){
 ?>
 
 <ul>
+  <?php #バーストした際にはドローができないように
+  if($isBurst == false){ ?>
   <li><a href="?draw">ドロー</a></li>
   <li><a href="?stand">確定</a></li>
+<?php } ?>
   <li><a href="?reset">最初から</a></li>
 </ul>
