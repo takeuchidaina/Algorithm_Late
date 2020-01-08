8898af9cb73cad1f9b72e4aa283336657b205f79
takeuchidaina <yda.takeuchidaina@gmail.com>
2020-01-08 09:23:07 +0900

    [update]ゲームとして遊べるように(初期化にバグあり)
    初期化でセッションがない場合に対応できていません。ページを更新するとなんとかなります。

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 session_start();
 
+#################
+#変数宣言
+#################
 $isGameEnd = false;     //true ゲーム終了 false 進行中
-$isBurst = false;       //true バースト false 21以下
+$isPlayerBurst = false; //true バースト false 21以下
+$isEnemyBurst = false;  //true バースト false 21以下
+$isPlayerStand = false; //true カード確定 false カード未確定
+$isEnemyStand = false;  //true カード確定 false カード未確定
 $deck   = array();      //デッキ
-$player  = array();     //手札
+$player  = array();     //プレイヤー
+$enemy = array();       //敵AI
 
 ##################################################
 #デッキ構築
@@ -29,12 +36,7 @@ function InitDeck(){
 
     return $deck;
 }
-##################################################
-#敵の行動
-##################################################
-function EnemyTurn(){
 
-}
 ##################################################
 #手札の点数
 ##################################################
@@ -68,54 +70,176 @@ function BurstCheck($_someOne){
 #################
 #初期化
 #################
+if(!isset($_GET['reset'])){
+    if(isset($_SESSION['deck']))         $deck  = $_SESSION['deck'];
+    if(isset($_SESSION['player']))       $player = $_SESSION['player'];
+    if(isset($_SESSION['enemy']))        $enemy    = $_SESSION['enemy'];
+  }
 if(isset($_GET['reset'])){
   $deck = InitDeck();               //デッキの生成
-  $player[] = array_shift($deck);   //1枚目ドロー
-  $player[] = array_shift($deck);   //2枚目ドロー
+  $player[] = array_shift($deck);   //プレイヤー1枚目ドロー
+  $player[] = array_shift($deck);   //プレイヤー2枚目ドロー
+  $enemy[] = array_shift($deck);    //敵1枚目ドロー
+  $enemy[] = array_shift($deck);    //敵2枚目ドロー
+
+  //$deck  = $_SESSION['deck'];
+  //$player = $_SESSION['player'];
+  //$enemy    = $_SESSION['enemy'];
 }
 //リセットされていないなら
 else{
+  $deck = InitDeck();               //デッキの生成
+  $player[] = array_shift($deck);   //プレイヤー1枚目ドロー
+  $player[] = array_shift($deck);   //プレイヤー2枚目ドロー
+  $enemy[] = array_shift($deck);    //敵1枚目ドロー
+  $enemy[] = array_shift($deck);    //敵2枚目ドロー
+
   $deck  = $_SESSION['deck'];
   $player = $_SESSION['player'];
+  $enemy    = $_SESSION['enemy'];
 }
 
 $_SESSION['deck']  = $deck;
 $_SESSION['player'] = $player;
+$_SESSION['enemy'] = $enemy;
 
 #################
 #リンク処理
 #################
+
 #カードを引く
 if(isset($_GET['draw'])){
   $player[] = array_shift($deck);     //3枚目以降ドロー
-  EnemyTurn();
+
+  //敵AI
+  //15以下ならカードを一枚引く
+  //プレイヤー
+  if((HandScore($enemy) <= 15 && $isPlayerBurst == false) ||
+        ($isPlayerStand == true && HandScore($enemy) < HandScore($player))){
+    $enemy[] = array_shift($deck);
+  }
+  //15以上なら確定
+  else{
+    $isEnemyStand = true;
+  }
 }
+
 #スタンド
 if(isset($_GET['stand'])){
+  $isPlayerStand = true;
 
+  //敵AI
+  if((HandScore($enemy) <= 15 && $isPlayerBurst == false) ||
+        ($isPlayerStand == true && HandScore($enemy) < HandScore($player))){
+    //15以上になるまでカードを引く
+    while(HandScore($enemy) <= 15 || HandScore($enemy) < HandScore($player)){
+      $enemy[] = array_shift($deck);
+    }
+    //バーストしていないなら確定状態へ
+    if(BurstCheck($enemy) == false){
+      $isEnemyStand = true;
+    }
+    else{
+      $isEnemyBurst = true;
+    }
+  }
+  else if(BurstCheck($enemy) == false){
+    $isEnemyStand = true;
+  }
 }
 
 //更新
 $_SESSION['deck']  = $deck;
 $_SESSION['player'] = $player;
+$_SESSION['enemy'] = $enemy;
 
 #################
-#手札を表示
+#プレイヤーの状況
 #################
-echo "【手札】SCORE:";
+echo "【P手札】SCORE:";
 $score = HandScore($player);
 echo $score."<br>";
 
 //バーストしているか確認
 if(BurstCheck($player) == true){
   echo "BURST<br>";
-  $isBurst = true;
+  $isPlayerBurst = true;
+}
+
+//確定しているか表示
+if($isPlayerStand == true){
+  echo "STAND<br>";
 }
 
 //手札の中身を表示
 foreach($player as $deck){
   echo $deck['num']."　　";
 }
+echo "<br>";
+
+#################
+#エネミーの状況
+#################
+echo "【E手札】SCORE:";
+$score = HandScore($enemy);
+echo $score."<br>";
+
+
+//バーストしているか確認
+if(BurstCheck($enemy) == true){
+  echo "BURST<br>";
+  $isEnemyBurst = true;
+}
+
+//確定しているか表示
+if($isEnemyStand == true){
+  echo "STAND<br>";
+}
+
+//手札の中身を表示
+//if($isGameEnd == true){
+  foreach($enemy as $deck){
+    echo $deck['num']."　　";
+  }
+//}
+
+
+echo "<br>";
+echo "<br>";
+
+#################
+#勝敗判定
+#################
+
+//バースト
+if($isPlayerBurst == false && $isEnemyBurst == true){
+  echo "PLAYER WIN";
+  $isGameEnd = true;
+}
+else if($isPlayerBurst == true && $isEnemyBurst == false){
+  echo "PLAYER LOSE";
+  $isGameEnd = true;
+}
+else if($isPlayerBurst == true && $isEnemyBurst == true){
+  echo "DRAW";
+  $isGameEnd = true;
+}
+
+//スタンド
+if($isPlayerStand == true && $isEnemyStand == true){
+  if(HandScore($player) > HandScore($enemy)){
+    echo "PLAYER WIN";
+  }
+  else if(HandScore($player) < HandScore($enemy)){
+    echo "PLAYER LOSE";
+  }
+  else{
+    echo "DRAW";
+  }
+  $isGameEnd = true;
+}
+
+
 ?>
 
 <?php
@@ -127,7 +251,7 @@ foreach($player as $deck){
 
 <ul>
   <?php #バーストした際にはドローができないように
-  if($isBurst == false){ ?>
+  if($isGameEnd == false){ ?>
   <li><a href="?draw">ドロー</a></li>
   <li><a href="?stand">確定</a></li>
 <?php } ?>
